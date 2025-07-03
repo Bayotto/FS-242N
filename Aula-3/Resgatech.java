71416577bfee855660957d111f0cf419a62614ff
João Pedro <jpappelt10@gmail.com>
2025-07-03 19:44:58 -0300

    Add files via upload

---
+++ Aula-3/Resgatech.java
@@ -0,0 +1,45 @@
+package P.I;
+
+import Conexao.ConexaoDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Resgatech {
+    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "root";
+
+
+    public static Connection conectar(){
+        Connection conexao = null;
+
+        try{
+            conexao = DriverManager.getConnection(URL,USUARIO,SENHA);
+            System.out.println("Conexão com o banco de dados estabelecida com sucesso");
+        }catch(SQLException error){
+            System.err.println("Erro ao conectar ao bd:" + error.getMessage());
+
+        } return conexao;
+    }
+    public static void fecharConexao(Connection conexao){
+        if(conexao != null){
+            try{
+                conexao.close();
+                System.out.println("Conexao com bd fechada!");
+
+            }catch (SQLException e){
+                System.err.println("Erro ao fechar a conexão com bd:" + e.getMessage());
+            }
+
+        }
+    }
+    public static void main(String[] args){
+        Connection testeConexao = ConexaoDB.conectar();
+        if(testeConexao != null){
+            ConexaoDB.fecharConexao(testeConexao);
+
+        }
+    }
+}
